35da5553377f1c5240a17a1fd43edc4289ebdc2b
paultarasenko <paultarasenko@gmail.com>
2026-05-13 13:29:40 +0300

    Update currency_bot.py

--- currency_bot.py
+++ currency_bot.py
@@ -31,6 +31,8 @@ import aiohttp
 
 import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN not set! Add it to environment variables.")
 
 #  Состояния диалога
 
